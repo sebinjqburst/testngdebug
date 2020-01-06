395c7f6e16c2c5ec6272cfccc3c948c4347870f9
qbuser <qbuser@Admins-iMac.local>
2020-01-06 12:44:21 +0530

    Initial commit

---
+++ testngDebug/src/test/java/com/debug/testngDebug/AppTestOne.java
@@ -0,0 +1,16 @@
+package com.debug.testngDebug;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTestOne
+{
+   
+	@Test
+	public void testOne() {
+		System.out.println("First Test");
+	}
+	
+}
---
+++ testngDebug/src/test/java/com/debug/testngDebug/AppTestTwo.java
@@ -0,0 +1,11 @@
+package com.debug.testngDebug;
+
+import org.testng.annotations.Test;
+
+public class AppTestTwo {
+	
+	@Test
+	public void testTwo() {
+		System.out.println("Second Test");
+	}
+}
